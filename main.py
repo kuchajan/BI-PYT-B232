b7f0ad924fed8ad4ebdd5ad8b58c58418bb98f2a
Jan Kuchař <kuchaj19@fit.cvut.cz>
2024-03-06 14:28:23 +0100

    Keep some notes on upcoming implementation

---
+++ main.py
@@ -0,0 +1,5 @@
+# Game state: Main menu, Play menu, Playing level, Level editor menu, Editing level
+
+# Main meWindow nu: Play, Level editor, Exit
+# Play menu: Choose level, Back
+# Level editor menu: Choose level, Create new, Back
