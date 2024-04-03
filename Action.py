ee2e2b686c8532efacb17eb0c39e0256d49c1468
Jan Kuchař <kuchaj19@fit.cvut.cz>
2024-04-03 14:32:57 +0200

    Reiterate Action and Level

--- Action.py
+++ Action.py
@@ -1,7 +1,9 @@
+"""Implements an enum class to represent user actions in level"""
 from enum import Enum
 
 
 class Action(Enum):
+    """Enum that represent user actions"""
     MOVE_UP = 0
     MOVE_RIGHT = 1
     MOVE_DOWN = 2
--- Level.py
+++ Level.py
@@ -1,37 +1,42 @@
-import pygame
+"""Class that represents an opened level the user is currently playing"""
+
+import numpy as np
+import pygame as pg
 
 from Action import Action
 
 
 class Level:
-    filepath = None
-    matrix = []
-    playerPosition = None
-    boxPositions = None
+    """Class that represents an opened level the user is currently playing"""
 
     def reload(self):
+        """Reloads a level from filepath"""
         preload = []
         cols = -1
-        with open(self.filepath, 'r') as f:
+        # TODO: Handle edge cases like non-existing file, file deleted mid read, forbidden access etc.
+        with open(self.filepath, 'r', encoding="utf-8") as f:
             for row in f.read().splitlines():
                 preload.append(list(row))
                 cols = cols if cols >= len(row) else len(row)
         rows = len(preload)
+        self.matrix = np.zeros((rows,cols),dtype=np.uint8)
 
         for row in range(rows):
-            row = []
             for col in range(cols):
-                row.append(' ')
-                # load from preload
-                # check for special symbols:
-                #
-                # select appropriate symbol
-            self.matrix.append(row)
+                currPos = ' ' if len(preload[row]) <= col else preload[row][col]
+                # All possible things
+                # #: wall
+                # @: player
+                # $: box
+                # .: dest
+                # P: Player on dest
+                # B: Box on dest
+                
 
-    def load(self, filepath):
-        self.filepath = filepath
+
+    def __init__(self, filePath):
+        self.filepath = filePath
+        self.matrix = np.zeros((0,0), dtype=np.uint8)
+        self.playerPosition = None
+        self.boxPositions = []
         self.reload()
-
-    def handle_action(self, action):
-        if action == Action.EXIT:
-            pygame.quit()
