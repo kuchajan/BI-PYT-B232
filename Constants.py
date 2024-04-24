2576608a25d57d8659760475a151ecedcd9caba9
Jan Kuchař <kuchaj19@fit.cvut.cz>
2024-04-24 11:48:50 +0200

    Finish implementation of handle_action in GameStatus

---
+++ Constants.py
@@ -0,0 +1,5 @@
+"""Definition of constants"""
+
+NOTHING = 0
+WALL = 1
+DESTINATION = 2
--- GameStatus.py
+++ GameStatus.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+import Constants as C
 from Action import Action
 
 class GameStatus:
@@ -9,6 +10,10 @@ class GameStatus:
     def __init__(self, playerPos: np.array, boxPos: set):
         self.playerPos = playerPos
         self.boxPos = boxPos.copy()
+
+    def copy(self):
+        """Creates a copy of the instance"""
+        return GameStatus(self.playerPos, self.boxPos)
 
     def handle_action(self, action: Action, matrix: np.array):
         """Handles a move action"""
@@ -25,16 +30,24 @@ class GameStatus:
         elif action == Action.MOVE_LEFT:
             # x--
             movevector[0] -= 1
+        else:
+            # invalid action
+            return self.copy()
 
-        newpos = (self.playerPos[0] + movevector[0], self.playerPos[1] + movevector[1])
-        if matrix[newpos[0]][newpos[1]] == 1: # attempted to move into a wall
-            return
-        
+        newpos = self.playerPos + movevector
+        if matrix[newpos[0]][newpos[1]] == C.WALL: # attempted to move into a wall
+            return self.copy()
+
         if newpos not in self.boxPos:
-            self.playerPos = newpos
-            return
-        
-        # there's a box
-        # todo: check if I'm attempting a move into a wall with box or into a box with box
-        # todo: handle calculation of new box set
+            return GameStatus(newpos, self.boxPos)
 
+        # there's a box
+        newSingleBoxPos = newpos + movevector
+        if matrix[newSingleBoxPos[0]][newSingleBoxPos[1]] == C.WALL or newSingleBoxPos in self.boxPos:
+            # moving box into wall or into box
+            return self.copy()
+
+        newBoxPos = self.boxPos.copy()
+        newBoxPos.remove(newpos)
+        newBoxPos.add(newSingleBoxPos)
+        return GameStatus(newpos, newBoxPos)
