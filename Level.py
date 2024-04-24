b12d2b63b659fddc0dd0eb24bf181d6d6a1fc143
Jan Kuchař <kuchaj19@fit.cvut.cz>
2024-04-24 12:10:48 +0200

    Implement bfs

--- Level.py
+++ Level.py
@@ -11,6 +11,31 @@ from Action import Action
 class Level:
     """Class that represents an opened level the user is currently playing"""
 
+    def is_end(self, gamestatus = None):
+        """Check that gamestatus is winning"""
+        # todo: check that every box in gamestatus are on dests
+        if (gamestatus is None):
+            # check instance gamestatus
+            return True
+        # check received gamestatus
+        return False
+
+    def bfs(self):
+        """Breadth first search that finds the optimal count of moves to solve level"""
+        visited = {}
+        visited[self.gamestatus.copy()] = 0
+        queue = [self.gamestatus.copy()]
+        while queue:
+            visiting = queue.pop(0)
+            if self.is_end(visiting):
+                return visited[visiting]
+            for action in [Action.MOVE_UP, Action.MOVE_RIGHT, Action.MOVE_DOWN, Action.MOVE_LEFT]:
+                toVisit = visiting.handle_action(action, self.matrix)
+                if toVisit not in visited:
+                    queue.append(toVisit)
+                    visited[toVisit] = visited[visiting] + 1
+        return -1
+
     def reload(self):
         """Reloads a level from filepath"""
         preload = []
@@ -63,8 +88,14 @@ class Level:
             raise ValueError("Level has incorrect count of objects")
         self.gamestatus = GameStatus(playerPos, boxPos)
 
+        self.optimalMoves = self.bfs()
+        if (self.optimalMoves == -1):
+            raise ValueError("Level is not solveable")
+
     def __init__(self, filePath):
         self.filepath = filePath
         self.matrix = np.zeros((0,0), dtype=np.uint8)
         self.gamestatus = GameStatus(np.zeros((2), np.int8), set())
+        self.moves = 0
+        self.optimalMoves = -1
         self.reload()
