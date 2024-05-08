b7b31e9627d281f4dbf89124d59c24db1d63ec1e
Jan Kuchař <kuchaj19@fit.cvut.cz>
2024-05-08 14:58:27 +0200

    Fix inconsistent usage of variable names

--- level.py
+++ level.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-import constants as Const
+import constants as const
 from game_status import GameStatus
 from action import Action
 
@@ -10,15 +10,14 @@ from action import Action
 class Level:
     """Class that represents an opened level the user is currently playing"""
 
-    def is_win(self, gamestatus: GameStatus = None):
-        """Check that gamestatus is winning"""
-        return self.gamestatus.box_pos == self.dests if gamestatus is None else gamestatus.box_pos == self.dests
+    def is_win(self, game_status: GameStatus = None):
+        """Check that game_status is winning"""
+        return self.game_status.box_pos == self.dests if game_status is None else game_status.box_pos == self.dests
 
     def bfs(self):
         """Breadth first search that finds the optimal count of moves to solve level"""
-        visited = {}
-        visited[self.gamestatus] = 0
-        queue = [self.gamestatus]
+        visited = {self.game_status: 0}
+        queue = [self.game_status]
         while queue:
             visiting = queue.pop(0)
             if self.is_win(visiting):
@@ -32,6 +31,8 @@ class Level:
 
     def reload(self):
         """Reloads a level from filepath"""
+        if not isinstance(self.filepath, str) or len(self.filepath) == 0:
+            raise ValueError("Cannot load level with given filepath")
         preload = []
         cols = -1
         # TODO: Handle edge cases like non-existing file, file deleted mid read, forbidden access etc.
@@ -40,11 +41,11 @@ class Level:
                 preload.append(row)
                 cols = cols if cols >= len(row) else len(row)
         rows = len(preload)
-        self.matrix = np.zeros((rows,cols),dtype=np.uint8)
+        self.matrix = np.zeros((rows, cols), dtype=np.uint8)
 
         player_cnt = 0
 
-        player_pos = np.zeros((2), np.int8)
+        player_pos = np.zeros(2, np.int8)
         box_pos = set()
         dests_tmp = set()
 
@@ -53,11 +54,11 @@ class Level:
                 curr_pos = ' ' if len(preload[row]) <= col else preload[row][col]
                 # nothing
                 if curr_pos == ' ':
-                    self.matrix[row][col] = Const.NOTHING
+                    self.matrix[row][col] = const.NOTHING
                     continue
                 # wall
                 if curr_pos == '#':
-                    self.matrix[row][col] = Const.WALL
+                    self.matrix[row][col] = const.WALL
                     continue
                 load_valid = False
                 # Player
@@ -72,14 +73,14 @@ class Level:
                 # Dest
                 if curr_pos in ".PB":
                     load_valid = True
-                    self.matrix[row][col] = Const.DESTINATION
+                    self.matrix[row][col] = const.DESTINATION
                     dests_tmp.add((row, col))
                 if not load_valid:
                     raise ValueError("Invalid character loaded from file")
 
-        if (player_cnt != 1 or len(box_pos) != len(dests_tmp)):
+        if player_cnt != 1 or len(box_pos) != len(dests_tmp):
             raise ValueError("Level has incorrect count of objects")
-        self.gamestatus = GameStatus(player_pos, box_pos)
+        self.game_status = GameStatus(player_pos, box_pos)
         self.dests = frozenset(dests_tmp)
 
         self.optimal_moves = self.bfs()
@@ -88,18 +89,26 @@ class Level:
 
     def handle_action(self, action: Action):
         """Handles an action from the user"""
-        if action == action.RESET:
+        if action == Action.RESET:
             self.reload()
             return
         if action in [Action.MOVE_UP, Action.MOVE_RIGHT, Action.MOVE_DOWN, Action.MOVE_LEFT]:
-            self.game_status = self.game_status.handle_action(action)
+            self.game_status = self.game_status.handle_action(action, self.matrix)
+            return
+        if action == Action.NOTHING:
             return
         raise ValueError("Invalid action passed to level")
 
-    def __init__(self, filepath):
+    def __init__(self, filepath: str = "", matrix=np.zeros((0, 0), dtype=np.uint8), dests=frozenset(),
+                 game_status=GameStatus(np.zeros(2, np.int8), set())):
         self.filepath = filepath
-        self.matrix = np.zeros((0,0), dtype=np.uint8)
-        self.dests = frozenset()
-        self.game_status = GameStatus(np.zeros((2), np.int8), set())
+        self.matrix = matrix
+        self.dests = dests.copy()
+        self.game_status = game_status.copy()
         self.optimal_moves = -1
-        self.reload()
+        if filepath != "":
+            self.reload()
+        else:
+            self.optimal_moves = self.bfs()
+            if self.optimal_moves == -1:
+                raise ValueError("Level is not solvable")
